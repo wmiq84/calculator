95f1ed0906f7622e23899136f69e4dd51082d4f4
wmiq84 <michaelwang3489@gmail.com>
2024-09-03 11:35:46 -0700

    Add logic for multi-number operations and update README.md

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # calculator
 
-Code for a basic calculator page using html, css, js. This is partially incomplete, at least according to the Odin Foundation guidelines, and doesn't have some functionality like adding more than two numbers. I plan to revisit this, and my other projects, after completing the Node course to fully complete them.
+Code for a basic calculator page using html, css, js. It's very barebones, and I plan to revisit this later.
+
+This was completed as the last project of The Odin Project Foundations.
--- app.js
+++ app.js
@@ -9,12 +9,14 @@ const equal = document.querySelector('#equal');
 const clear = document.querySelector('#clear');
 const disp = document.querySelector('#display');
 
+
+// display and stroe numerical values
 nums.forEach((num) => {
     num.addEventListener('click', (event) => {
         const clickedNumber = event.target.innerText;
         if (!opClicked) {
             firstNumber += clickedNumber;
-            disp.innerText = firstNumber;
+            disp.innerText += clickedNumber;
         } else {
             secondNumber += clickedNumber;
             disp.innerText = firstNumber + secondNumber;
@@ -22,9 +24,17 @@ nums.forEach((num) => {
     });
 });
 
+
+
 operators.forEach((op) => {
     op.addEventListener('click', (event) => {
-        if (firstNumber !== '') {
+        //performs operation pre-adding two numbers if 3+ numbers are selected
+        if (opClicked === true) {
+            equal.dispatchEvent(new Event('click'));
+            operator = event.target.innerText;
+            disp.innerText += operator;
+        }
+        else{ 
             opClicked = true;
             operator = event.target.innerText;
         }
@@ -35,6 +45,8 @@ equal.addEventListener('click', () => {
     if (firstNumber !== '' && secondNumber !== '' && operator !== null) {
         const result = operate(parseFloat(firstNumber), parseFloat(secondNumber), operator);
         disp.innerText = result;
+
+        //sets the first number to operation result
         firstNumber = result.toString();
         secondNumber = '';
         operator = null;

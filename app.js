32738c3f18c176307947513b72cc607743dc35fa
wmiq84 <michaelwang3489@gmail.com>
2024-09-02 17:23:56 -0700

    Initialize files and add operator functions to app.js

---
+++ app.js
@@ -0,0 +1,28 @@
+
+let firstNumber = 1;
+let secondNumber = 2;
+let operator = "+";
+
+
+
+
+function add() {
+
+}
+
+function subtract() {
+
+}
+
+function multiply() {
+
+}
+
+function divide() {
+
+}
+
+function operator(firstNumber, secondNumber, operator) {
+    switch (operator) {
+    }
+} 
